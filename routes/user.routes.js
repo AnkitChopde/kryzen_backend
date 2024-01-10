01430b700458c8d49dbe0ea8b141c7da8512bc6a
Ankesh Chopde <acchopde84@gmail.com>
2024-01-10 09:24:30 +0530

    added form api

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { userModel } = require("../models/user.model");
 const bcrypt = require("bcrypt")
 const userRoutes = express.Router();
-
+const jwt = require("jsonwebtoken")
 
 userRoutes.post("/register",async(req,res)=>{
     const {username,password} = req.body;
